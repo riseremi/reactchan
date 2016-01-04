a6ee6833b495021d5116470236f49019b54cc542
riseremi <riseremi@icloud.com>
2016-01-04 22:03:10 +0000

    feat: responsive version

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class IndexView extends React.Component {
 
 	render() {
-		return <div style={{color: '#090E00', fontFamily: 'serif', display: 'table', border: '1px solid #F9E0A8', background: 'none repeat scroll 0% 0% #FFECB2', borderRadius: '3px', marginTop: '4px', minWidth: '380px'}}>
+		return <div className='post-wrapper' style={{color: '#090E00', fontFamily: 'serif', display: 'table', border: '1px solid #F9E0A8', background: 'none repeat scroll 0% 0% #FFECB2', borderRadius: '3px', marginTop: '4px', minWidth: '380px'}}>
 			<label>
 				<input type="checkbox"/>
 				<span className="postername" style={{fontFamily: 'sans-serif'}}>Аноним&nbsp;</span>
