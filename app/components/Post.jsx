134d1c46dec177a10ef28520a7f7e5938d8c89e4
riseremi <riseremi@icloud.com>
2016-01-04 18:26:48 +0000

    feat: post component

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export default class IndexView extends React.Component {
 	
 	render() {
-		return <h1>Index Page</h1>;
+		return <div style={{border: '1px solid #ccc', borderRadius: '3px', marginBottom: '30px'}}>
+			<span>{this.props.reply.username}</span>
+			<span>{new Date(this.props.reply.timestamp).toString()}</span>
+			<span><a>#{this.props.reply.id}</a></span>
+			
+			<p>{this.props.reply.text}</p>
+		</div>;
 	}
 	
 }
