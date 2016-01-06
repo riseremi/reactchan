6f0e5619905639599e2c0568e372f36bbfcb66d6
riseremi <riseremi@icloud.com>
2016-01-06 08:41:07 +0000

    feat: post date time displaying

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DateFormatter from '../utils/DateFormatter';
 
 export default class IndexView extends React.Component {
 
@@ -7,13 +8,13 @@ export default class IndexView extends React.Component {
 			<label>
 				<input type="checkbox"/>
 				<span className="postername" style={{fontFamily: 'sans-serif'}}>Няноним&nbsp;</span>
-				<span className="time">xx.xx.xxx (xx) xx:xx:xx&nbsp;</span>
+				<span className="time">{DateFormatter.getPostDate(this.props.post.timestamp)} {DateFormatter.getPostTime(this.props.post.timestamp)}&nbsp;</span>
 			</label>
 			<span className="reflink">
 				<a style={{textDecoration: 'none', color: '#090E00'}} href={'#' + this.props.post.id}>No.&nbsp;</a>
 				<a style={{textDecoration: 'none', color: '#090E00'}} href={'#' + this.props.post.id}>{this.props.post.id}</a>
 			</span>
-			
+
 			<div className='postbody'>
 				<blockquote style={{whiteSpace: 'pre-line'}}>{this.props.post.text}</blockquote>
 			</div>
