d74583e5ee134320e046ab403ed5f0db58f2ab61
riseremi <riseremi@icloud.com>
2016-01-06 09:49:45 +0000

    feat: replace plain ajax calls with lib

--- app/views/ThreadView.jsx
+++ app/views/ThreadView.jsx
@@ -1,5 +1,8 @@
+/* global humane */
+
 import React from 'react';
 import Post from '../components/Post';
+import request from 'superagent';
 
 export default class ThreadView extends React.Component {
 
@@ -27,46 +30,31 @@ export default class ThreadView extends React.Component {
 	}
 
 	updatePosts() {
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = () => {
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-				console.log('responseText:' + xmlhttp.responseText);
-				try {
-					var data = JSON.parse(xmlhttp.responseText);
-				}
-				catch (err) {
-					console.log(err.message + " in " + xmlhttp.responseText);
-					return;
-				}
-				// callback(data);
-				this.setState({
-					posts: data
-				});
-				humane.log('Updated', {
-					timeout: 1000
-				});
-			}
+		let callback = (err, res) => {
+			this.setState({
+				posts: res.body
+			});
+			humane.log('Updated', {
+				timeout: 1000
+			});
+			console.log('[AJAX] - get posts, response body:', res.body);
 		};
 
-		xmlhttp.open("GET", 'http://chan-riseremi.c9users.io/posts', true);
-		xmlhttp.send();
+		request('GET', 'http://chan-riseremi.c9users.io/posts').end(callback);
 	}
 
 	sendPostHandler() {
-		let requestJSON = {
-			// oh magic
-			id: Math.floor(Math.random() * (1488 * 1488 - 0 + 1)) + 0,
+		let data = {
 			text: document.getElementById('text').value
 		};
 
-		var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
-		xmlhttp.open("POST", "http://chan-riseremi.c9users.io/posts");
-		xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-		xmlhttp.send(JSON.stringify(requestJSON));
+		let callback = (err, res) => {
+			console.log('posting:', data);
+			this.updatePosts();
+			document.getElementById('text').value = '';
+		};
 
-		console.log('posting:', requestJSON);
-		this.updatePosts();
-		document.getElementById('text').value = '';
+		request.post('http://chan-riseremi.c9users.io/posts').send(data).end(callback);
 	}
 
 	autoUpdateHandler(event) {
@@ -98,32 +86,34 @@ export default class ThreadView extends React.Component {
 		if (!autoUpdate) {
 			clearInterval(this.state.timer);
 			clearInterval(this.state.counter);
-			this.setState({timeOut: TIMEOUT});
+			this.setState({
+				timeOut: TIMEOUT
+			});
 			document.getElementById('autoUpdate-text').innerHTML = 'Автообновление';
 		}
 	}
 
 	render() {
 		return <div>
-		
+
 			<textarea id='text' cols='48' rows='4' placeholder='Your message'/>
 			<br/>
 			<button onClick={this.sendPostHandler.bind(this)}>Send</button>
 			&nbsp;
 			<button className='refresh' onClick={this.updatePosts.bind(this)}>Refresh</button>
 			&nbsp;
-			
+
 			<label className="postername" style={{fontFamily: 'sans-serif'}}>
 				<input type="checkbox" onChange={this.autoUpdateHandler.bind(this)}/>
 				<span id='autoUpdate-text'>Автообновление</span>
 			</label>
-			
+
 			{
 				this.state.posts.map((post) => {
 					return <Post key={post.id} post={post}/>;
 				})
 			}
-		
+
 		</div>;
 	}
 }
