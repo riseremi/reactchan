2cb3a6d5be39e3df4c7a5b6075c3c32a2d576845
riseremi <riseremi@icloud.com>
2016-01-07 21:48:32 +0000

    feat: new boards

--- app/Layout.jsx
+++ app/Layout.jsx
@@ -6,7 +6,7 @@ export default class Layout extends React.Component {
 	render() {
 		return (
 			<div>
-				<Link to="/test/">/test/</Link>
+				[<Link to="/dev">dev</Link> / <Link to="/beta">beta</Link>]
 				<h1><Link to="/">reactchan :3</Link></h1>
 				<div>{this.props.children}</div>
 			</div>
